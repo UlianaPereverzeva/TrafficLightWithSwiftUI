59b376f5e47466a711481006560b305261b07dae
UlianaPereverzeva <115875780+UlianaPereverzeva@users.noreply.github.com>
2023-02-13 18:38:29 +0300

    project created

---
+++ TrafficLightWithSwiftUI/TrafficLightWithSwiftUI/ContentView.swift
@@ -0,0 +1,27 @@
+//
+//  ContentView.swift
+//  TrafficLightWithSwiftUI
+//
+//  Created by ульяна on 13.02.23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+            
+        }
+        .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TrafficLightWithSwiftUI/TrafficLightWithSwiftUI/TrafficLightWithSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TrafficLightWithSwiftUIApp.swift
+//  TrafficLightWithSwiftUI
+//
+//  Created by ульяна on 13.02.23.
+//
+
+import SwiftUI
+
+@main
+struct TrafficLightWithSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
